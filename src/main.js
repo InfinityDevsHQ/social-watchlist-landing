3caa3dae483f918e5073f10f1052edfd14c5a319
Danish <danishdevv@users.noreply.github.com>
2024-05-10 17:58:56 +0500

    fix responsive

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// document.querySelector(".burger").addEventListener("click", function () {
+//   document.querySelector(".nav-links").classList.toggle("nav-active");
+//   this.classList.toggle("toggle");
+// });
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  // navLinks.classList.toggle("block");
+  // navLinks.classList.toggle("hidden");
+  navLinks.classList.toggle("active");
+});
