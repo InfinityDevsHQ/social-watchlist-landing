9a20cd9e2255f51b4ebeb9a9048327c19f0f0d72
Danish <danishdevv@users.noreply.github.com>
2024-05-10 20:35:11 +0500

    fix(nav): fixed responsive nav for mobile

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@
 //   this.classList.toggle("toggle");
 // });
 
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
+// const burger = document.querySelector(".burger");
+// const navLinks = document.querySelector(".nav-links");
 
-navLinks.addEventListener("click", () => {
-  // navLinks.classList.toggle("block");
-  // navLinks.classList.toggle("hidden");
-  navLinks.classList.toggle("hidden");
-  navLinks.classList.toggle("flex");
+// navLinks.addEventListener("click", () => {
+//   // navLinks.classList.toggle("block");
+//   // navLinks.classList.toggle("hidden");
+//   navLinks.classList.toggle("hidden");
+//   navLinks.classList.toggle("flex");
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.querySelector(".mobile-menu-button");
+  const menu = document.querySelector(".mobile-menu");
+
+  menuButton.addEventListener("click", function () {
+    menu.classList.toggle("hidden");
+  });
 });
